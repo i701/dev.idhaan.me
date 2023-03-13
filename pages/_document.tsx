e9f29fe59cae8dbe0527028cb50eafa1e5fb1552
i701 <idhaan701@outlook.com>
2023-03-13 16:51:40 +0500

    dark mode transparency for content section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      {/* TODO: fix bg for dark and light mode */}
+      <body id="content" className="dark:bg-transparent">
         <Main />
         <NextScript />
       </body>
