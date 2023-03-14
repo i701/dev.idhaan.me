a2225ebcb6654ca729f9b06ce902f3c711a72d04
i701 <idhaan701@outlook.com>
2023-03-14 22:58:41 +0500

    update timeline component to accept props

--- pages/components/TimelineEvent.tsx
+++ pages/components/TimelineEvent.tsx
@@ -1,21 +1,61 @@
-import React from "react"
+import {
+  CalendarIcon,
+  AcademicCapIcon,
+  MapPinIcon,
+  BuildingOfficeIcon,
+} from "@heroicons/react/24/solid"
 
-const TimelineEvent = () => {
+export interface TimelineProps {
+  id?: number
+  date: string
+  educationName: string
+  educatedPlaceName: string
+  educatedPlaceLink: string
+  location: string
+  eventSummary: string
+}
+
+const TimelineEvent = ({
+  date,
+  educationName,
+  educatedPlaceName,
+  educatedPlaceLink,
+  location,
+  eventSummary,
+}: TimelineProps) => {
   return (
     <div className="relative hover:border-orange-500 dark:hover:border-gray-400 border-l-4 border-gray-700/20 py-8 transition duration-150">
       <div className="linedot"></div>
-      <div className="pl-10 space-y-2">
-        <span className="text-xs md:text-base tracking-widest font-bold dark:text-orange-500 ">
-          16 Nov 2021
-        </span>
-        <h3 className="md:text-3xl text-gray-600 font-semibold">
-          Diploma of Information Technlology
-        </h3>
-        <p className="text-sm">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
-          dolor quidem rerum amet aliquid enim inventore nostrum expedita
-          eveniet? Nihil impedit blanditiis officiis exercitationem.
-        </p>
+      <div className="pl-10 space-y-2 md:space-y-4">
+        <div className=" flex gap-2 items-center pb-4">
+          <CalendarIcon className="h-5 w-5 text-gray-600" />
+          <p className="text-xs md:text-base tracking-widest font-bold dark:text-orange-500">
+            {date}
+          </p>
+        </div>
+        <div className=" flex gap-2 items-center">
+          <AcademicCapIcon className="aspect-square text-gray-400 linedot bg-white dark:text-gray-600 dark:bg-black" />
+          <h3 className="md:text-xl text-sm text-gray-600 font-semibold">
+            {educationName}
+          </h3>
+        </div>
+        <div className=" flex gap-2 items-center">
+          <BuildingOfficeIcon className="aspect-square text-gray-400 linedot bg-white dark:text-gray-600 dark:bg-black" />
+          <h3 className="md:text-xl text-sm text-gray-600">
+            {educatedPlaceName}
+          </h3>
+        </div>
+        <div className=" flex gap-2 items-center">
+          <MapPinIcon className="aspect-square text-gray-400 linedot bg-white dark:text-gray-600 dark:bg-black" />
+          <a
+            target={"_blank"}
+            href={educatedPlaceLink}
+            className="md:text-xl text-xs text-gray-600 hover:text-orange-600"
+          >
+            {location}
+          </a>
+        </div>
+        <p className="text-xs md:text-base">{eventSummary}</p>
       </div>
     </div>
   )
