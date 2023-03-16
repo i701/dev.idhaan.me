928ca3cb293dc284f16e0f7e86f45208f7b9e07e
i701 <idhaan701@outlook.com>
2023-03-16 15:18:47 +0500

    refactor mdx components

---
+++ pages/components/MDXComponents/index.tsx
@@ -0,0 +1,10 @@
+import Pre from "./Pre"
+import Code from "./Code"
+import CodeTitle from "./CodeTitle"
+const MDXComponents = {
+  pre: Pre,
+  code: Code,
+  CodeTitle,
+}
+
+export default MDXComponents
