bdf30fd07cd4b15d5ada56c6645de4908264ad5a
i701 <idhaan701@outlook.com>
2023-03-18 00:10:10 +0500

    add homepage with demo blogs and search functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
 import { getAllPosts, PostMeta } from "@/src/postApi"
 import Head from "next/head"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import Postcard from "./components/Postcard"
 
 export default function Home({ posts }: { posts: PostMeta[] }) {
+  const [searchValue, setSearchValue] = useState("")
+  const [filteredPosts, setFilteredPosts] = useState([...posts])
+  const searchRef = useRef<HTMLInputElement>(null!)
+
+  useEffect(() => {
+    setFilteredPosts(
+      posts.filter((post) =>
+        post.title.toLowerCase().includes(searchValue.trim().toLowerCase())
+      )
+    )
+  }, [searchValue, posts])
+
+  function handleAutoSearch(e: any) {
+    if (e.code === "Slash" && e.ctrlKey) {
+      searchRef.current.focus()
+    }
+  }
+  useEffect(() => {
+    document.addEventListener("keydown", handleAutoSearch)
+
+    return () => document.removeEventListener("keydown", handleAutoSearch)
+  }, [])
+
   return (
     <>
       <Head>
@@ -16,11 +40,22 @@ export default function Home({ posts }: { posts: PostMeta[] }) {
           <h1 className="text-2xl md:text-4xl font-bold text-orange-600">
             <span className="accent-line ">Hello </span>
           </h1>
-          <p className="dark:text-gray-400 md:text-base text-sm">
-            Welcome to by blog!
-          </p>
+          <div className="flex justify-between items-center flex-wrap gap-2">
+            <p className="dark:text-gray-400 md:text-base text-sm">
+              Welcome to by blog!
+            </p>
 
-          {posts.map((post) => (
+            <input
+              ref={searchRef}
+              className="dark:bg-transparent border dark:border-gray-600 border-gray-200 rounded px-2 py-2 w-full md:w-1/2 focus:outline-none focus:!border-orange-600"
+              type="text"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Press (CTRL + /) to search... "
+            />
+          </div>
+
+          {filteredPosts.map((post) => (
             <Postcard key={post.title} post={post} />
           ))}
         </div>
