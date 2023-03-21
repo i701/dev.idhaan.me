b2e2821b117f767e3215ae73b0f2e53cc2d8985a
i701 <idhaan701@outlook.com>
2023-03-21 19:11:08 +0500

    add spotify response types

--- lib/types.ts
+++ lib/types.ts
@@ -17,3 +17,78 @@ export interface PostMeta {
   tags?: string[]
   date: string
 }
+
+export interface TItems {
+  items: Item[]
+}
+
+export interface Item {
+  track: Track
+  played_at: Date
+  context: null
+}
+
+export interface Track {
+  album: Album
+  artists: Artist[]
+  available_markets: string[]
+  disc_number: number
+  duration_ms: number
+  explicit: boolean
+  external_ids: ExternalIDS
+  external_urls: ExternalUrls
+  href: string
+  id: string
+  is_local: boolean
+  name: string
+  popularity: number
+  preview_url: string
+  track_number: number
+  type: string
+  uri: string
+}
+
+export interface Album {
+  album_group: string
+  album_type: string
+  artists: Artist[]
+  available_markets: string[]
+  external_urls: ExternalUrls
+  href: string
+  id: string
+  images: Image[]
+  is_playable: boolean
+  name: string
+  release_date: Date
+  release_date_precision: string
+  total_tracks: number
+  type: string
+  uri: string
+}
+
+export interface Artist {
+  external_urls: ExternalUrls
+  href: string
+  id: string
+  name: string
+  type: Type
+  uri: string
+}
+
+export interface ExternalUrls {
+  spotify: string
+}
+
+export enum Type {
+  Artist = "artist",
+}
+
+export interface Image {
+  height: number
+  url: string
+  width: number
+}
+
+export interface ExternalIDS {
+  isrc: string
+}
