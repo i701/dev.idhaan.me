2ce69abe09eee86a7d0adbdd77ba26d1adca9bb0
i701 <idhaan701@outlook.com>
2023-03-23 16:46:27 +0500

    add theme switching sound

--- pages/components/ThemeButton.tsx
+++ pages/components/ThemeButton.tsx
@@ -1,15 +1,21 @@
 import { useTheme } from "next-themes"
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid"
+import useSound from "use-sound"
 
 const ThemeButton = () => {
+  const soundUrl = "/switch.wav"
+  const [play, { stop }] = useSound(soundUrl, { volume: 0.4 })
   const { resolvedTheme, setTheme } = useTheme()
 
   return (
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="flex items-center justify-center rounded-lg p-2 transition-all"
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="flex items-center justify-center rounded-lg p-2 transition-all focus:outline-none focus:ring-2 focus:ring-orange-500"
+      onClick={() => {
+        play()
+        setTheme(resolvedTheme === "dark" ? "light" : "dark")
+      }}
     >
       {resolvedTheme === "dark" ? (
         <SunIcon className="h-5 w-5 text-orange-300" />
