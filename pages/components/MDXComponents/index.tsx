a21b5efc2cdf009df2e8246109b3a321f0554061
i701 <idhaan701@outlook.com>
2023-03-23 16:48:34 +0500

    export custom blockquote

--- pages/components/MDXComponents/index.tsx
+++ pages/components/MDXComponents/index.tsx
@@ -1,9 +1,11 @@
 import Pre from "./Pre"
 import Code from "./Code"
 import CodeTitle from "./CodeTitle"
+import Blockquote from "./Blockquote"
 const MDXComponents = {
   pre: Pre,
   code: Code,
+  blockquote: Blockquote,
   CodeTitle,
 }
 
