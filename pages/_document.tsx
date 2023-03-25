984e6302691c8c0dffb32041daa2dfeb34de555a
i701 <idhaan701@outlook.com>
2023-03-25 16:26:44 +0500

    RTL

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" dir="ltr">
       <Head />
       {/* TODO: fix bg for dark and light mode */}
       <body id="content" className="dark:bg-transparent">
