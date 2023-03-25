5e37a9219cf12766d735948ac21f441152d8676a
i701 <idhaan701@outlook.com>
2023-03-25 16:29:48 +0500

    update OG background image

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -6,52 +6,42 @@ export const config = {
 }
 
 export default async function handler(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url)
-    const hasTitle = searchParams.has("title")
-    const title = hasTitle
-      ? searchParams.get("title")?.slice(0, 100)
-      : "λ701's Blog"
-    return new ImageResponse(
-      (
+  const { searchParams } = req.nextUrl
+  const postTitle = searchParams.get("title")
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundImage: "url(https://dev.idhaan.me/og-background.png)",
+        }}
+      >
         <div
           style={{
-            height: "100%",
-            width: "100%",
+            marginLeft: 190,
+            marginRight: 190,
             display: "flex",
-            flexDirection: "column",
-            fontFamily: '"Outfit"',
-            alignItems: "flex-start",
-            justifyContent: "center",
-            backgroundImage: "url(http://dev.idhaan.me/og-background.png)",
+            fontSize: 130,
+            letterSpacing: "-0.05em",
+            fontStyle: "normal",
+            color: "white",
+            lineHeight: "120px",
+            whiteSpace: "pre-wrap",
           }}
         >
-          <div
-            style={{
-              marginLeft: 110,
-              marginRight: 190,
-              display: "flex",
-              fontSize: 100,
-              letterSpacing: "-0.05em",
-              fontStyle: "bold",
-              color: "white",
-              lineHeight: "120px",
-              whiteSpace: "pre-wrap",
-            }}
-          >
-            {title}
-          </div>
+          {postTitle}
         </div>
-      ),
-      {
-        width: 1980,
-        height: 1080,
-      }
-    )
-  } catch (e: any) {
-    console.log(`${e.message}`)
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    })
-  }
+      </div>
+    ),
+    {
+      width: 1920,
+      height: 1080,
+    }
+  )
 }
