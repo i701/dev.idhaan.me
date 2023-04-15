c8932ef8a95e7547b72988ced5f5046960e3636c
i701 <idhaan701@outlook.com>
2023-04-15 17:28:13 +0500

    change font

--- lib/MDXComponents/Code.tsx
+++ lib/MDXComponents/Code.tsx
@@ -1,12 +1,17 @@
+import { Space_Mono } from "next/font/google"
+
 type Props = {
   children?: string | React.ReactNode
 }
+const space = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] })
 
 export default function Code(props: Props) {
   return (
     <>
       {typeof props.children === "string" ? (
-        <code className="!bg-gray-30 text-gray-800 dark:text-gray-200 font-bold rounded before:text-gray-500 after:text-gray-500">
+        <code
+          className={`${space.className}!bg-gray-500 font-medium text-gray-800 dark:text-gray-200 rounded before:text-gray-500 after:text-gray-500`}
+        >
           `{props.children}`
         </code>
       ) : (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar"
 import NProgress from "../components/Progress"
 import { ThemeProvider } from "next-themes"
 
-import { Space_Mono } from "next/font/google"
+import { Space_Mono, Outfit, Inter } from "next/font/google"
 import { SWRConfig } from "swr"
 import ErrorBoundary from "../components/ErrorBoundary"
 import { useState } from "react"
@@ -12,7 +12,7 @@ import { Router } from "next/router"
 import Loading from "../components/Loading"
 import { Analytics } from "@vercel/analytics/react"
 
-const space = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] })
+const outfit = Outfit({ weight: ["400", "700"], subsets: ["latin"] })
 
 export default function App({ Component, pageProps }: AppProps) {
   const [pageLoading, setPageLoading] = useState<boolean>(false)
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <ThemeProvider attribute="class">
           <Navbar />
           <main
-            className={`${space.className} max-w-5xl min-h-screen mx-auto pb-12 px-4 text-gray-600 dark:text-gray-400 antialiased pt-20`}
+            className={`${outfit.className} max-w-5xl min-h-screen mx-auto pb-12 px-4 text-gray-600 dark:text-gray-400 antialiased pt-20`}
           >
             <div className="fixed -z-10 inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
             <SWRConfig
