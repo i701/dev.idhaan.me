7ad0b28ca2f55597dc6fef1e4dcc8611d4f7bc90
i701 <idhaan701@outlook.com>
2023-05-01 22:33:46 +0500

    change font to grotesk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar"
 import NProgress from "../components/Progress"
 import { ThemeProvider } from "next-themes"
 
-import { Space_Mono } from "next/font/google"
+import { Space_Grotesk, Space_Mono } from "next/font/google"
 import { SWRConfig } from "swr"
 import ErrorBoundary from "../components/ErrorBoundary"
 import { useState } from "react"
@@ -12,7 +12,10 @@ import { Router } from "next/router"
 import Loading from "../components/Loading"
 import { Analytics } from "@vercel/analytics/react"
 
-const space = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] })
+const spacegrotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   const [pageLoading, setPageLoading] = useState<boolean>(false)
@@ -29,9 +32,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <ThemeProvider attribute="class">
           <Navbar />
           <main
-            className={`${space.className} max-w-5xl min-h-screen mx-auto pb-12 px-4 text-gray-600 dark:text-gray-400 antialiased pt-20`}
+            className={`${spacegrotesk.className} max-w-3xl min-h-screen mx-auto pb-12 px-4 text-gray-600 dark:text-gray-400 antialiased pt-20`}
           >
-            <div className="fixed -z-10 inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
             <SWRConfig
               value={{
                 fetcher: async <JSON = any,>(
