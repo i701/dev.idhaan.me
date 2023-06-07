3aa4011db11feccfe098f0e786ea3115204064e7
i701 <idhaan701@outlook.com>
2023-06-07 22:07:43 +0500

    update slug page to include avatar banner, add progressbar, add view vounter and add social share icons

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -5,13 +5,49 @@ import { serialize } from "next-mdx-remote/serialize"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import rehypeHighlight from "rehype-highlight"
-// import YouTube from "@/src/components/youTube";
 import "highlight.js/styles/atom-one-dark.css"
 import { MDXPost } from "@/lib/types"
 import MDXContent from "@/lib/MDXContent"
 import { MDXComponents } from "@/lib/MDXComponents"
 
-const PostPage = ({ post }: { post: MDXPost }) => {
+import localFont from "next/font/local"
+import { useEffect, useRef, useState } from "react"
+const typewriterNormal = localFont({
+  src: "../../public/fonts/MVTypewriter_reg.ttf",
+})
+const typewriterBold = localFont({
+  src: "../../public/fonts/MVTypewriter_bol.ttf",
+})
+import kv from "@vercel/kv"
+
+import { Space_Grotesk } from "next/font/google"
+import { useRouter } from "next/router"
+import CoolAvatarBanner from "@/components/CoolAvatarBanner"
+import ScrollToTopButton from "@/components/ScrollToTop"
+import ArticleProgressBar from "@/components/ProgressBar"
+import SocialShare from "@/components/SocialShare"
+const spacegrotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+})
+
+const PostPage = ({
+  post,
+  views,
+  FULL_URL,
+}: {
+  post: MDXPost
+  views: number
+  FULL_URL: string
+}) => {
+  const [isDv, setIsDv] = useState<boolean>(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (post.meta.dv === true) {
+      setIsDv(true)
+    }
+  }, [post.meta.dv])
   return (
     <>
       <Head>
@@ -21,15 +57,26 @@ const PostPage = ({ post }: { post: MDXPost }) => {
           content={`https://dev.idhaan.me/api/og?title=${post.meta.title}`}
         />
       </Head>
-      <div className="max-w-5xl pt-4 px-4 mx-auto">
-        <div className="text-center mb-8">
-          <h1 className="mt-4 text-4xl text-black dark:text-zinc-400 font-medium sm:text-5xl md:text-6xl md:leading-normal sm:leading-normal">
+      <ArticleProgressBar />
+      <div className="max-w-xl pt-4 px-4 mx-auto">
+        <div
+          className={`${
+            isDv ? "text-right" : "text-center"
+          } mb-8 flex flex-col items-center justify-center`}
+        >
+          <h1
+            className={`
+            ${isDv === true ? typewriterBold.className : spacegrotesk.className}
+            ${isDv === true ? "leading-relaxed" : ""}
+             mt-4 text-3xl md:text-4xl text-black dark:text-zinc-400 font-bold md:leading-loose sm:leading-normal`}
+          >
             <span>{post.meta.title}</span>
+            {/* <pre>{JSON.stringify({ isDv }, null, 2)}</pre> */}
           </h1>
           <div className="flex items-center justify-center mt-4">
-            <div className="flex items-center">
-              <div className="ml-2">
-                <p className="text-sm text-gray-500 dark:text-gray-400">
+            <div className="border dark:border-gray-700 p-2 rounded text-gray-500 dark:text-gray-400 text-sm flex gap-2 items-center">
+              <div className="">
+                <p>
                   Published on{" "}
                   {new Date(post.meta.date).toLocaleDateString("en-US", {
                     month: "short",
@@ -38,33 +85,60 @@ const PostPage = ({ post }: { post: MDXPost }) => {
                   })}{" "}
                 </p>
               </div>
+              |
+              <div className="border dark:border-gray-700 rounded px-2 dark:bg-orange-500/20 bg-orange-100 flex gap-2 items-center">
+                <p className="">views - </p>
+                <p className="text-orange-500 font-bold">
+                  {Intl.NumberFormat("en-us").format(views)}
+                </p>
+              </div>
             </div>
+          </div>
+          <div>
+            <CoolAvatarBanner />
           </div>
         </div>
       </div>
       <article
-        className="prose-sm md:prose-lg
+        className={`${
+          isDv === true ? typewriterNormal.className : ""
+        } prose-sm md:prose-lg max-w-xl mx-auto
         prose-pre:!m-0
         prose-li:list-disc
+        prose-ul:list-inside
         prose-li:leading-loose
-        prose-li:text-[1rem]
         prose-a:no-underline
         prose-a:font-bold
         prose-code:font-mono
         prose-h1:text-base
         prose-headings:leading-normal
-        prose-p:leading-loose prose-img:rounded-xl
-        prose-p:text-[1rem]
-        "
+        prose-img:rounded
+        prose-img:my-0
+        prose-video:my-0
+        prose-p:leading-loose
+        ${
+          isDv === true
+            ? "md:prose-p:text- prose-p:text-[20px] prose-li:text-[20px]"
+            : "prose-p:text-[1rem] prose-li:text-[1rem]"
+        }
+        ${isDv === true ? "text-right tracking-wider" : ""}
+        `}
+        style={{
+          direction: `${isDv === true ? "rtl" : "ltr"}`,
+        }}
       >
         <MDXRemote {...post.source} components={MDXComponents} />
+        <ScrollToTopButton />
+        <SocialShare full_url={FULL_URL} />
       </article>
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const BASE_URL = `http://dev.idhaan.me/post/`
   const { slug } = params as { slug: string }
+  const FULL_URL = BASE_URL + slug
   const { content, meta } = await new MDXContent("posts").getPostFromSlug(slug)
   const mdxSource = await serialize(content, {
     mdxOptions: {
@@ -76,7 +150,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     },
   })
 
-  return { props: { post: { source: mdxSource, meta } } }
+  const views = await kv.get(`${slug}`)
+  if (!views) {
+    await kv.set(`${slug}`, "0")
+  }
+  await kv.incr(`${slug}`)
+
+  return {
+    props: {
+      FULL_URL,
+      post: { source: mdxSource, meta },
+      views,
+    },
+  }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
