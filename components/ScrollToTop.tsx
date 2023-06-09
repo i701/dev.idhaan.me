e3b33e62be70ae61c5d33bc35687d035f0738e21
i701 <idhaan701@outlook.com>
2023-06-10 00:53:07 +0500

    make adjustments to make scroll to top component prettier

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -5,7 +5,7 @@ import {
   Variants,
 } from "framer-motion"
 import { useEffect } from "react"
-import { ArrowLongUpIcon } from "@heroicons/react/24/solid"
+import { ArrowSmallUpIcon, ChevronUpIcon } from "@heroicons/react/24/solid"
 
 const ScrollToTopContainerVariants: Variants = {
   hide: { opacity: 0 },
@@ -35,13 +35,13 @@ function ScrollToTopButton() {
 
   return (
     <motion.button
-      className="fixed shadow-2xl bg-white bottom-10 left-5 dark:bg-black dark:ring-orange-500 dark:ring-2 transition  rounded-full p-4"
+      className="fixed shadow-2xl bg-persian-orange/50 dark:bg-persian-orange/10 backdrop-blur-lg  bottom-10 left-5 ring-orange-500 ring-2 transition rounded-full p-3"
       variants={ScrollToTopContainerVariants}
       initial="hide"
       animate={controls}
       onClick={scrollToTop}
     >
-      <ArrowLongUpIcon className="h-6 w-6 text-orange-500" />
+      <ArrowSmallUpIcon className="h-6 w-6 text-orange-500" />
     </motion.button>
   )
 }
