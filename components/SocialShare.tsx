bc0ad18b77960cd05806dd5da84e71df507c4056
i701 <idhaan701@outlook.com>
2023-06-09 18:42:40 +0500

    update social share component

--- components/SocialShare.tsx
+++ components/SocialShare.tsx
@@ -13,25 +13,25 @@ export default function SocialShare({ full_url }: { full_url: string }) {
       {/* <p className="text-xs text-orange-900">share this article</p> */}
       <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
         <FacebookShareButton url={full_url}>
-          <div className="border text-sm rounded gap-2 flex items-center justify-between p-2">
+          <div className="border dark:border-gray-700 text-sm rounded gap-2 flex items-center justify-between p-2">
             <span>Share</span>
             <SocialIcon style={{ height: 20, width: 20 }} network="facebook" />
           </div>
         </FacebookShareButton>
         <TelegramShareButton url={full_url}>
-          <div className="border text-sm rounded gap-2 flex items-center justify-between p-2">
+          <div className="border dark:border-gray-700 text-sm rounded gap-2 flex items-center justify-between p-2">
             <span>Send</span>
             <SocialIcon style={{ height: 20, width: 20 }} network="telegram" />
           </div>
         </TelegramShareButton>
         <TwitterShareButton url={full_url}>
-          <div className="border text-sm rounded gap-2 flex items-center justify-between p-2">
+          <div className="border dark:border-gray-700 text-sm rounded gap-2 flex items-center justify-between p-2">
             <span>Tweet</span>
             <SocialIcon style={{ height: 20, width: 20 }} network="twitter" />
           </div>
         </TwitterShareButton>
         <WhatsappShareButton url={full_url}>
-          <div className="border text-sm rounded gap-2 flex items-center justify-between p-2">
+          <div className="border dark:border-gray-700 text-sm rounded gap-2 flex items-center justify-between p-2">
             <span>Send</span>
             <SocialIcon style={{ height: 20, width: 20 }} network="whatsapp" />
           </div>
