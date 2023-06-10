1d62906baef4d366113e87b0140edfd7905f11f7
i701 <idhaan701@outlook.com>
2023-06-10 19:23:43 +0500

    clearn up document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="en" dir="ltr">
+    <Html lang="en">
       <Head />
-      {/* TODO: fix bg for dark and light mode */}
-      <body id="content" className="dark:bg-transparent">
+      <body id="content">
         <Main />
         <NextScript />
       </body>
