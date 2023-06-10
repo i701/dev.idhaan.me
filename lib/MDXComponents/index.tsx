0106e224761056c739fec8d826c4d659e96778c7
i701 <idhaan701@outlook.com>
2023-06-10 19:55:16 +0500

    export MediaBox for MDXComponent

--- lib/MDXComponents/index.tsx
+++ lib/MDXComponents/index.tsx
@@ -3,10 +3,14 @@ import Code from "./Code"
 import CodeTitle from "./CodeTitle"
 import Blockquote from "./Blockquote"
 import Dua from "./Dua"
+import MediaBox from "./MediaBox"
+
 export const MDXComponents = {
   pre: Pre,
   code: Code,
   blockquote: Blockquote,
   CodeTitle,
   Dua,
+  MediaBox,
+  Blockquote,
 }
