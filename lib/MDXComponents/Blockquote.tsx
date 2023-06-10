f8ecf8523d59a9a5b5e2072a8868f5dffe112462
i701 <idhaan701@outlook.com>
2023-06-10 19:52:15 +0500

    update MDX blockquote for rtl and ltr support

--- lib/MDXComponents/Blockquote.tsx
+++ lib/MDXComponents/Blockquote.tsx
@@ -1,11 +1,18 @@
 type Props = {
   children?: string | React.ReactNode
+  isDv?: boolean
 }
 
 export default function Blockquote(props: Props) {
   return (
     <>
-      <blockquote className="md:text-lg text-orange-500 bg-persian-orange/20 rounded-sm pr-4 py-4  border-l-orange-500 border-l-2">
+      <blockquote
+        className={`md:text-lg  pr-4  ${
+          props.isDv
+            ? "border-r-2 mr-[.10rem] border-orange-500/10"
+            : "border-orange-500 bg-persian-orange/20 border-l-2 text-orange-500"
+        }`}
+      >
         {props.children}
       </blockquote>
     </>
